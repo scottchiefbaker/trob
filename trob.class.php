d44ece6d474a27def4f86ca6751cc540d4a37a4d
Scott Baker <scott@perturb.org>
2020-06-16 08:58:26 -0700

    Move files to the examples dir

--- trob.class.php
+++ trob.class.php
@@ -6,29 +6,7 @@ require("$conf_dir/include/smarty/libs/Smarty.class.php");
 require("$conf_dir/include/krumo/class.krumo.php");
 require("$conf_dir/include/db_query/db_query.class.php");
 
-// Borrowed from : https://github.com/igorw/get-in
-// Discussion    : http://nikic.github.io/2014/01/10/The-case-against-the-ifsetor-function.html
-function get_in(array $array, array $keys, $default = null) {
-	if (!$keys) {
-		return $array;
-	}
-
-	// This is a micro-optimization, it is fast for non-nested keys, but fails for null values
-	if (count($keys) === 1 && isset($array[$keys[0]])) {
-		return $array[$keys[0]];
-	}
-	$current = $array;
-	foreach ($keys as $key) {
-		if (!array_key_exists($key, $current)) {
-			return $default;
-		}
-		$current = $current[$key];
-	}
-
-	return $current;
-}
-
-class page {
+class trob {
 	public $require_https = false;
 	public $require_login = false;
 	public $plugins       = array();
