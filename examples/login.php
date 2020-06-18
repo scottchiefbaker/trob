2a7a99e29e99a1d3703edd5b84dd3afb5f78c22b
Scott Baker <scott@perturb.org>
2020-06-18 10:26:37 -0700

    Login page cleanups

--- examples/login.php
+++ examples/login.php
@@ -1,37 +1,35 @@
-<?PHP
+<?php
 
-require('easy_page/easy_page.class.php');
-$p = new page();
+require('/home/bakers/github/trob/trob.class.php');
+$trob = new trob();
 
 if ($_POST['username']) { process_login($_POST); }
-if ($_GET['logout']) { 
-	session_destroy(); 
+if ($_GET['logout']) {
+	session_destroy();
 	header("Location: login.php");
 }
 
-$p->display();
+$trob->display();
 
-#################################################################
+///////////////////////////////////////////////////////////////////
 
 function process_login($i) {
-	$un = $_POST['username'];
+	$un  = $_POST['username'];
 	$pwd = $_POST['password'];
 
 	if (is_valid_login($un,$pwd)) {
 		header("Location: index.php");
 	}
 
-	global $p;
-	$p->assign('error','Username/Password not correct');
+	global $trob;
+	$trob->assign('error','Username/Password not correct');
 }
 
-function is_valid_login($un,$pwd) {
-	global $p;
+function is_valid_login($un,$trobwd) {
+	global $trob;
 
 	$_SESSION['username'] = $un;
-	$_SESSION['cust_id'] = $data[0]['CustID'];
+	$_SESSION['cust_id']  = $data[0]['CustID'];
 
 	return true;
 }
-
-?>
