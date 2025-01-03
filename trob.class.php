8c19d18df6aa93bed891ba75f06e99294ad80a0d
Scott Baker <scott@perturb.org>
2025-01-03 10:12:47 -0800

    Add a version string

--- trob.class.php
+++ trob.class.php
@@ -24,6 +24,7 @@ class trob {
 	public $skin_dir      = "";
 	public $smarty        = "";
 	public $end_time      = "";
+	public $version       = 0.7;
 
 	function __construct($opts = array()) {
 		$this->start_time  = microtime(1);
